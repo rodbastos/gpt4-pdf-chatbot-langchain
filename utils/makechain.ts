1dfe0f0ce911be7e4059f34024e85f99c7be5cad
rodbastos <rodrigo@targetteal.com>
2023-04-25 09:42:21 -0300

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -23,7 +23,7 @@ export const makeChain = (vectorstore: PineconeStore) => {
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
-    vectorstore.asRetriever(),
+    vectorstore.asRetriever(2),
     {
       qaTemplate: QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
